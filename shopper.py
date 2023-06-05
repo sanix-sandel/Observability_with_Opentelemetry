e67b2a83932f099f8f7a5c6b192477f464c07fae
Sanix <sanick.ikani@rakuten.com>
2023-06-06 05:20:05 +0900

    add resource attribute

--- shopper.py
+++ shopper.py
@@ -1,19 +1,25 @@
 #! /usr/bin/env python3
-from opentelemetry import trace, context
+from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, BatchSpanProcessor
+from opentelemetry.sdk.resources import Resource
 
-
-def configure_tracer():
+def configure_tracer(name, version):
     exporter = ConsoleSpanExporter()
     span_processor = BatchSpanProcessor(exporter)
-    provider = TracerProvider()
+    resource = Resource.create(
+        {
+            "service.name": name,
+            "service.version": version
+        }
+    )
+    provider = TracerProvider(resource=resource)
     provider.add_span_processor(span_processor)
     trace.set_tracer_provider(provider)
-    return trace.get_tracer("shopper.py", "0.0.1")
+    return trace.get_tracer(name, version)
 
 
-tracer = configure_tracer()
+tracer = configure_tracer("shopper", "0.1.2")
 
 
 @tracer.start_as_current_span("browse")
