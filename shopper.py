381fa9748d04743fc84c00b833a5a22d2b71fdf3
Sanix <sanick.ikani@rakuten.com>
2023-06-06 04:45:03 +0900

    attach context

---
+++ shopper.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+from opentelemetry import trace, context
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+
+def configure_tracer():
+    exporter = ConsoleSpanExporter()
+    span_processor = SimpleSpanProcessor(exporter)
+    provider = TracerProvider()
+    provider.add_span_processor(span_processor)
+    trace.set_tracer_provider(provider)
+    return trace.get_tracer("shopper.py", "0.0.1")
+
+
+def browse():
+    print("Visiting the grocery store")
+
+
+if __name__ == "__main__":
+    tracer = configure_tracer()
+    span = tracer.start_span("Visit store")
+    ctx = trace.set_span_in_context(span)
+    token = context.attach(ctx)
+    span2 = tracer.start_span("browse")
+    browse()
+    span2.end()
+    context.detach(token)
+    span.end()
